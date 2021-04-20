05c70f7fc6f84366c08a4422624058678894b160
Dilarady <>
2021-04-20 11:22:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+class Main {
+  private static int[]aufstellung = {1, 3, 5, 7, 8, 10, 11, 13, 14, 16, 19};
+  
+  public static boolean binaereSuche(int[] pArray, int pSuchschluessel){
+    int links, rechts, mitte;
+    return false;
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    for (int i = 0 ; i < aufstellung.length; i++){
+      System.out.println(aufstellung[i]);
+    }
+
+  }
+}
