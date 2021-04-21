a710cdb984bd086b261d9129fc1efefc231a4920
Dilarady <>
2021-04-21 06:43:13 +0000

    "aufgestellt", "nicht aufgestellt"

--- Main.java
+++ Main.java
@@ -1,21 +1,35 @@
+
 class Main {
+  private static int[] aufstellung = { 1, 3, 5, 7, 8, 10, 11, 13, 14, 16, 19 };
+
   public static void main(String[] args) {
     System.out.println("Hello world!");
-  }
-}
-class Main {
-  private static int[]aufstellung = {1, 3, 5, 7, 8, 10, 11, 13, 14, 16, 19};
-  
-  public static boolean binaereSuche(int[] pArray, int pSuchschluessel){
-    int links, rechts, mitte;
-    return false;
-  }
-  
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-    for (int i = 0 ; i < aufstellung.length; i++){
+    for (int i = 0; i < aufstellung.length; i++) {
       System.out.println(aufstellung[i]);
     }
+    binaereSuche(aufstellung, 6);
 
   }
+
+  public static boolean binaereSuche(int[] a, int suchschlüssel) {
+    int links, rechts, mitte;
+    links = 0;
+    rechts = aufstellung.length - 1;
+    while (links <= rechts) {
+      mitte = (rechts + links) / 2;
+      if (a[mitte] == suchschlüssel) {
+        System.out.println("Ist aufgestellt");
+        return true;
+      }
+      if (a[mitte] < suchschlüssel) {
+        links = mitte + 1;
+      }
+      if (a[mitte] > suchschlüssel) {
+        rechts = mitte - 1;
+      }
+    }
+    System.out.println("Ist nicht aufgestellt");
+    return false;
+  }
+
 }
